a8dc708393cabdac19a7ea0295ec4a9a3ceedad2
Jae Gangemi <jgangemi@gmail.com>
2026-04-22 14:28:37 -0600

    chore: adopt breadboard v0.11.7 (#148)

--- scripts/fetch_breadboard.py
+++ scripts/fetch_breadboard.py
@@ -1,7 +1,7 @@
 Import("env")
 import os, subprocess
 
-VERSION = "v0.11.6"
+VERSION = "v0.11.7"
 DEST = os.path.join(env.subst("$PROJECT_DIR"), ".breadboard")
 LOCAL = os.environ.get("BREADBOARD_LOCAL")
 

32bc746acf31a7e5ca7cb4811f17e5586c79237b
Jae Gangemi <jgangemi@gmail.com>
2026-04-21 18:48:29 -0600

    refactor: adopt bb_nv_flash_init wrapper

--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <inttypes.h>
 #include "esp_log.h"
-#include "nvs_flash.h"
 #include "esp_system.h"
 #include "esp_sntp.h"
 #include "bb_wifi.h"
@@ -275,12 +274,7 @@ void app_main(void)
     ESP_ERROR_CHECK(bb_log_stream_init());
 
     // Initialize NVS (required by WiFi)
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
+    ESP_ERROR_CHECK(bb_nv_flash_init());
 
     // Load config from NVS (falls back to defaults)
     ESP_ERROR_CHECK(bb_nv_config_init());

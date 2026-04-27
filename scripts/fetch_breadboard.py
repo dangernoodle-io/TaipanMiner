3825f666bdaf8979e5ac6b74809025eeaa6f7c1f
Jae Gangemi <jgangemi@gmail.com>
2026-04-26 21:48:51 -0600

    chore: bump breadboard to v0.16.3

--- scripts/fetch_breadboard.py
+++ scripts/fetch_breadboard.py
@@ -1,7 +1,7 @@
 Import("env")
 import os, subprocess
 
-VERSION = "v0.16.2"
+VERSION = "v0.16.3"
 DEST = os.path.join(env.subst("$PROJECT_DIR"), ".breadboard")
 LOCAL = os.environ.get("BREADBOARD_LOCAL")
 
